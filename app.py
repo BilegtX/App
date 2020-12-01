190f66e2779faa63adbe391e685c01eec1bce284
BilegtX <74945721+BilegtX@users.noreply.github.com>
2020-12-01 13:22:59 +0800

    dajgui shu

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/niilber/<int:number1>/<int:niilber2>')
+def niilber(number1, number2):
+    return 'Sum: %d' + (number1 + number2)
